c5bd48a36180732dc357dca08dc56e109e54ccdf
Emilio Aguiar <junioraguiar_83@hotmail.com>
2024-04-23 00:17:54 +0100

    pdf to markdown

---
+++ akaquery/params.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+import ipdb
+
+##################  VARIABLES  ##################
+os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
+os.environ["ANTHROPIC_API_KEY"] = os.getenv('ANTHROPIC_API_KEY')
+#os.environ["MATHPIX_API_ID"] = os.getenv('MATHPIX_API_KEY')
