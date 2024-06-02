05a664533a55bd7bb052d81e443f29909ba1bea2
Emilio Aguiar <junioraguiar_83@hotmail.com>
2024-06-02 17:12:14 +0100

    streamlit issues v9

--- akaquery/interface/main.py
+++ akaquery/interface/main.py
@@ -3,7 +3,7 @@
 #import pysqlite3
 __import__('pysqlite3')
 import sys
-
+import sqlite3
 sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 import os
@@ -175,37 +175,17 @@ else:
     )  # use similarity search
 
 
-# Chat model stream handler
-class StreamHandler(BaseCallbackHandler):
-    def __init__(
-        self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""
-    ):
-        self.container = container
-        self.text = initial_text
-        self.run_id_ignore_token = None
-
-    def on_llm_start(self, serialized: dict, prompts: list, **kwargs):
-        # Workaround to prevent showing the rephrased question as output
-        if prompts[0].startswith("Human"):
-            self.run_id_ignore_token = kwargs.get("run_id")
-
-    def on_llm_new_token(self, token: str, **kwargs) -> None:
-        if self.run_id_ignore_token == kwargs.get("run_id", False):
-            return
-        self.text += token
-        self.container.markdown(self.text)
-
-
 # Retrieval handler
 class PrintRetrievalHandler(BaseCallbackHandler):
     def __init__(self, container, msgs, calculate_similarity=False):
-        self.status = container.status("**Context Retrieval**")
+        self.status_container = container
+        self.status = container.empty()  # Create an empty container
         self.msgs = msgs
         self.embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
         self.calculate_similarity = calculate_similarity
 
     def on_retriever_start(self, serialized: dict, query: str, **kwargs):
-        self.status.update(label=f"**Context Retrieval:** {query}")
+        self.status.markdown(f"**Context Retrieval:** {query}")
         self.msgs.add_ai_message(f"Query: {query}")
         if self.calculate_similarity:
             self.query_embedding = self.embeddings.embed_query(query)
@@ -229,10 +209,10 @@ class PrintRetrievalHandler(BaseCallbackHandler):
 
             source_msg = f"# Retrieval {idx+1}\n* **Document: {source}{page_txt}**{similarity_txt}\n\n {contents}\n\n"
 
-            self.status.write(source_msg, unsafe_allow_html=True)
+            self.status.markdown(source_msg, unsafe_allow_html=True)
             source_msgs += source_msg
         self.msgs.add_ai_message(source_msgs)
-        self.status.update(state="complete")
+        self.status.markdown("**Retrieval complete**")
 
     def cosine_similarity(self, embedding1, embedding2):
         return np.dot(embedding1, embedding2) / (
